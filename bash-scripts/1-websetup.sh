793cc1aa8dbe116dbd182335c74146a7cffbcc56
Sebastián Cáceres Gelvez <sebastian.caceres.g@gmail.com>
2024-01-11 13:16:57 -0500

    new bash files

---
+++ bash-scripts/1-websetup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Install Dependencies
+echo "#####################################"
+echo "Installing Packages"
+echo "#####################################"
+sudo yum install wget unzip httpd -y > /dev/null
+echo
+
+# Start & Enable Service
+echo "#####################################"
+echo "Start & Enable HTTPD Service"
+echo "#####################################"
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo
+
+# Create Temp. Directory
+echo "#####################################"
+echo "Start Artifact Deployment"
+echo "#####################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
+unzip 2098_health.zip > /dev/null
+sudo cp -r 2098_health/* /var/www/html/
+echo
+
+# Bounce Service
+echo "#####################################"
+echo "Restarting HTTPD Service"
+echo "#####################################"
+systemctl restart httpd
+echo
+
+# Clean Up
+echo "#####################################"
+echo "Removing Temporary Files"
+echo "#####################################"
+rm -rf /tmp/webfile
+echo
+
+sudo systemctl status httpd
+ls /var/www/html/
